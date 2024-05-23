729c76fc043a6cd23c2301d3c6d981d4e22f87fb
CarlosPerez19 <151254507+CarlosPerez19@users.noreply.github.com>
2024-05-23 08:18:21 -0500

    Nuevaclase

---
+++ Estudiantes/src/CLASEPRUEBA.java
@@ -0,0 +1,19 @@
+public class CLASEPRUEBA {
+
+    String Prueba;
+
+    public CLASEPRUEBA() {
+    }
+
+    public CLASEPRUEBA(String prueba) {
+        Prueba = prueba;
+    }
+
+    public String getPrueba() {
+        return Prueba;
+    }
+
+    public void setPrueba(String prueba) {
+        Prueba = prueba;
+    }
+}
